5b48c8b41ca594dcbe7d3d3d9ae58e6fd7938433
Benjamin NIDDAM <benjamin@weaverize.com>
2025-06-09 15:39:43 +0200

    Improves env handling and build config for production
    Removes the example environment file and updates ignore rules to exclude all env files from version control, reducing risk of leaking sensitive config. Enhances build scripts to explicitly use production mode and adds a separate production build command. Refactors configuration to support environment variables more robustly and defines a production mode flag for better environment-aware code.

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  return {
+    // base: '/app/',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    envDir: './',
+    envPrefix: 'VITE_',
+    define: {
+      __PROD__: mode === 'production',
+    },
+  }
 })
